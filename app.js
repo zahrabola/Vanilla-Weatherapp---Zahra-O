29dd2cdb300df9f1f528caf9894884c724301119
zahrabola <95353397+zahrabola@users.noreply.github.com>
2021-12-08 16:57:03 +0000

    Update app.js
    update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/-----------------------
+//-----------------------
 
 function formatTime(currentTime) {
   let currentHours = currentTime.getHours();
@@ -27,7 +27,31 @@ function formatTime(currentTime) {
 let formattedDate = document.querySelector(".DayandTime");
 formattedDate.innerHTML = formatTime(new Date());
 
- //------------------------------
+//------------------
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
